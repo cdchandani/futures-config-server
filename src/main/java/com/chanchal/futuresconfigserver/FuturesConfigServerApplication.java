0b2554101b117dec72dde49806529edbd141fc9e
Chanchal D Chandani <2021mt93760@wilp.bits-pilani.ac.in>
2023-11-17 02:06:07 +0530

    Initial commit

---
+++ src/main/java/com/chanchal/futuresconfigserver/FuturesConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.chanchal.futuresconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class FuturesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturesConfigServerApplication.class, args);
+	}
+
+}
